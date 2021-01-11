11fda174e191dac7ac3b2ae52573825a957bca0f
david <david@davidwbrown.name>
2021-01-10 23:32:31 -0600

    Refactored index() and added methods: getAdd and getDeduct

--- src/main/java/name/davidwbrown/PayersController.java
+++ src/main/java/name/davidwbrown/PayersController.java
@@ -5,18 +5,41 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 
+import java.text.ParseException;
 import java.util.List;
 
-@Controller("/payers")
+
+@Controller("/balance")
 public class PayersController {
+
     final BeanContext context = BeanContext.run();
-    Payers payers = context.getBean(Payers.class);
+    Payers payer = context.getBean(Payers.class);
     PayerAccountBean account = context.getBean(PayerAccountBean.class);
 
     @Get(produces = MediaType.TEXT_PLAIN)
-    public List<Integer> index() {
-        account.init();
-        return payers.getPoints();
+    public String index() throws ParseException {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < payer.getName().size(); i++) {
+            sb.append(payer.getName().get(i));
+            sb.append(" ");
+            sb.append(payer.getPoints().get(i));
+            sb.append(" ");
+            sb.append(payer.getDate().get(i));
+            sb.append(", ");
+        }
+        return sb.toString();
     }
+
+    @Get(produces = MediaType.TEXT_PLAIN, value = "/add/{+name}/{points}/{+date}")
+    String getAdd(String name, @QueryValue String points, @QueryValue String date) throws ParseException {
+        return account.getAdd(name, points, date);
+    }
+
+    @Get(produces = MediaType.TEXT_PLAIN, value = "/deduct/{+name}/{points}")
+    String getDeduct(String name, @QueryValue String points) {
+        return account.getDeduct(name, points);
+    }
+
 }
